5a954fcffe6159576af8069c2b2fb647298c82ed
ihubenov <ivaylo.hubenov@gmail.com>
2017-05-05 12:26:56 +0300

    updating client

--- BusTicketClient/src/client/BusTicketClient.java
+++ BusTicketClient/src/client/BusTicketClient.java
@@ -11,27 +11,25 @@ public class BusTicketClient {
 	private final static String HOST = "localhost";
 
 	public static void main(String[] args) throws IOException {
-		try  {
+		try {
 			@SuppressWarnings("resource")
 			Socket socket = new Socket(HOST, PORT);
 
-			new Thread(() -> {
-				try (Scanner chat = new Scanner(System.in);
-						Scanner in = new Scanner(socket.getInputStream());
-						PrintStream out = new PrintStream(socket.getOutputStream());) {
+			try (Scanner chat = new Scanner(System.in);
+					Scanner in = new Scanner(socket.getInputStream());
+					PrintStream out = new PrintStream(socket.getOutputStream());) {
 
-					System.out.println("Enter name: ");
-					String name = chat.nextLine();
-					out.println(name);
-					out.flush();
-					if (in.hasNext()) {
-						System.out.println(in.nextLine());
-					}
-
-				} catch (IOException e) {
-					e.printStackTrace();
+				System.out.println("Enter name: ");
+				String name = chat.nextLine();
+				out.println(name);
+				out.flush();
+				if (in.hasNext()) {
+					System.out.println(in.nextLine());
 				}
-			}).start();
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 
 		} catch (IOException noServer) {
 			System.out.println("The server might not be up at this time.");
